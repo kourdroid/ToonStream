26ad8a9cee167d75737045ecf279189d31793f22
Kourdroid <kourdroid@gmail.com>
2024-06-17 00:47:20 +0100

    try

--- toon_stream/asgi.py
+++ toon_stream/asgi.py
@@ -13,4 +13,5 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toonstream.settings")
 
-application = get_asgi_application()
+app = get_asgi_application()
+
