8b34660cb1c921b7e1c3ed05ebe49a0fe1d1ec9a
Kourdroid <kourdroid@gmail.com>
2024-07-04 00:03:38 +0100

    fix some issues

--- toon_stream/settings.py
+++ toon_stream/settings.py
@@ -120,4 +120,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_REDIRECT_URL = '/signup'
+LOGIN_URL = '/login'
